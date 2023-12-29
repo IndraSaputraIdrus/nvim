b9f10588bf20e1e3164bab63a2353a2efdf9be8f
indra <indrasaputraidrus@gmail.com>
2023-12-29 15:20:13 +0800

    feat: replace linter from nvim-lint to none-ls

--- lua/plugins/linter.lua
+++ lua/plugins/linter.lua
@@ -1,30 +1,23 @@
 return {
-  "mfussenegger/nvim-lint",
-  event = { "BufWritePost" },
+  "nvimtools/none-ls.nvim",
+  event = { "BufReadPre", "BufNewFile" },
   config = function()
-    local lint = require("lint")
+    local null_ls = require("null-ls")
+    local null_ls_utils = require("null-ls.utils")
+    local diagnostics = null_ls.builtins.diagnostics -- to setup linters
 
-    lint.
-    linters_by_ft = {
-      javascript = { "eslint_d" },
-      javascriptreact = { "eslint_d" },
-      typescript = { "eslint_d" },
-      typescriptreact = { "eslint_d" },
-      svelte = { "eslint_d" }
-    }
-
-
-    vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter", "InsertLeave" }, {
-      callback = function()
-        lint.try_lint()
-      end,
+    -- configure null_ls
+    null_ls.setup({
+      -- add package.json as identifier for root (for typescript monorepos)
+      root_dir = null_ls_utils.root_pattern(".null-ls-root", "Makefile", ".git", "package.json"),
+      -- setup formatters & linters
+      sources = {
+        diagnostics.eslint_d.with({                                           -- js/ts linter
+          condition = function(utils)
+            return utils.root_has_file({ ".eslintrc.js", ".eslintrc.cjs" })   -- only enable if root has .eslintrc.js or .eslintrc.cjs
+          end,
+        }),
+      }
     })
-
-    vim.keymap.set("n",
-      "<leader>l",
-      function()
-        lint.try_lint()
-      end
-      , { desc = "Toggle linter" })
   end
 }
