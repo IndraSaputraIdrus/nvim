39ae19b6fc1338858f2dcf130c32eadbcc70f47c
IndraSaputraIdrus <indrasaputraidrus@gmail.com>
2026-07-08 00:12:35 +0800

    install treesitter plugin

---
+++ plugin/treesitter.lua
@@ -0,0 +1,20 @@
+-- Auto build on update
+vim.api.nvim_create_autocmd("PackChanged", {
+    callback = function(ev)
+        local name = ev.data.spec.name
+        if name == "nvim-treesitter" then
+            vim.cmd("TSUpdate")
+        end
+    end,
+})
+
+vim.pack.add({
+    "https://github.com/nvim-treesitter/nvim-treesitter",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "<filetype>" },
+    callback = function()
+        vim.treesitter.start()
+    end
+})
