99dce583f2dad8361bd7bfb5bd4c55e0e05a5a71
IndraSaputraIdrus <indrasaputraidrus@gmail.com>
2026-07-08 19:23:58 +0800

    mini picker

--- plugin/mini.lua
+++ plugin/mini.lua
@@ -11,8 +11,43 @@ require("mini.indentscope").setup()
 require("mini.pairs").setup()
 require("mini.surround").setup()
 require("mini.statusline").setup()
+require("mini.extra").setup()
 
--- mini files
-local mini_files = require("mini.files")
-mini_files.setup()
-vim.keymap.set("n", "-", mini_files.open, { desc = "File manager" })
+vim.schedule(function()
+	-- mini files
+	require("mini.files").setup()
+	vim.keymap.set("n", "-", "<cmd>lua MiniFiles.open()<cr>", { desc = "File manager" })
+
+	-- mini pick
+	require("mini.pick").setup()
+
+	vim.keymap.set("n", "<leader>ff", "<cmd>Pick files tool='rg'<cr>", { desc = "Open file picker" })
+
+	vim.keymap.set(
+		"n",
+		"<leader>fd",
+		"<cmd>lua MiniExtra.pickers.diagnostic()<cr>",
+		{ desc = "Open diagnostic picker" }
+	)
+
+	vim.keymap.set(
+		"n",
+		"<leader>fgd",
+		"<cmd>lua MiniExtra.pickers.lsp({ scope = 'declaration' })<cr>",
+		{ desc = "Open diagnostic picker" }
+	)
+
+	vim.keymap.set(
+		"n",
+		"<leader>fgD",
+		"<cmd>lua MiniExtra.pickers.lsp({ scope = 'definition' })<cr>",
+		{ desc = "Open diagnostic picker" }
+	)
+
+	vim.keymap.set(
+		"n",
+		"<leader>fgi",
+		"<cmd>lua MiniExtra.pickers.lsp({ scope = 'implementation' })<cr>",
+		{ desc = "Open diagnostic picker" }
+	)
+end)
